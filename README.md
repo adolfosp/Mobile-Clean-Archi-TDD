eee154d6b8f9559ceb490ec24e5461222c55661d
Adolfo da Silveira Poiatti <adolfododo@hotmail.com>
2023-10-09 12:19:19 -0300

    move production code to their own target

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 This project was made to improve my skills on the dart
 
 # https://github.com/rmanguinho/clean-flutter-app/blob/master/requirements/use_cases/remote_authentication.md
+
+
+# Anotações de aprendizado
+
+- Sempre criar um arquivo 'index' com o nome da pasta e nele conterá todos os arquivos daquela pasta exportados 
+- importação de pacotes: quando estamos em pastas diferentes ex: lib, test, ai usamos package:, caso não usamos ../
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,22 +1,11 @@
 import 'package:faker/faker.dart';
+import 'package:mobile_clean_tdd/data/http/http.dart';
+import 'package:mobile_clean_tdd/data/usecases/usescases.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:mobile_clean_tdd/domain/usecases/authentication.dart';
 
-class RemoteAuthentication {
-  final HttpClient httpClient;
-  final String url;
 
-  RemoteAuthentication({required this.httpClient, required this.url});
-  Future<void> auth(AuthenticationParams params) async {
-    final body = {'email': params.email, 'password': params.secret};
-    await httpClient.request(url: url, method: 'post', body: body);
-  }
-}
-
-abstract class HttpClient {
-  Future<void> request({required String url, required String method, Map body});
-}
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
