14e8926dd21098c30dc95604e4d930525b6e19e9
Adolfo da Silveira Poiatti <adolfododo@hotmail.com>
2023-10-09 07:28:29 -0300

    ensure remoteAuthentication calls httpclient with correct body

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,26 +1,26 @@
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
+import 'package:mobile_clean_tdd/domain/usecases/authentication.dart';
 
-class RemoteAuthentication{
+class RemoteAuthentication {
   final HttpClient httpClient;
   final String url;
 
   RemoteAuthentication({required this.httpClient, required this.url});
-  Future<void> auth() async{
-    await httpClient.request(url: url, method: 'post');
+  Future<void> auth(AuthenticationParams params) async {
+    final body = {'email': params.email, 'password': params.secret};
+    await httpClient.request(url: url, method: 'post', body: body);
   }
 }
 
-abstract class HttpClient{
-  Future<void> request({ required String url, required String method});
+abstract class HttpClient {
+  Future<void> request({required String url, required String method, Map body});
 }
 
-class HttpClientSpy extends Mock implements HttpClient{
-}
+class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-
   late RemoteAuthentication sut;
   late HttpClientSpy httpClient;
   late String url;
@@ -31,11 +31,16 @@ void main() {
     sut = RemoteAuthentication(httpClient: httpClient, url: url);
   });
 
-  test('should call http client with correct values', () async{
+  test('should call http client with correct values', () async {
     //Act
-    await sut.auth();
+    final params = AuthenticationParams(
+        email: faker.internet.email(), secret: faker.internet.password());
+    await sut.auth(params);
 
     //Assert
-    verify(httpClient.request(url: url, method: 'post')).called(1);
+    verify(httpClient.request(
+        url: url,
+        method: 'post',
+        body: {'email': params.email, 'password': params.secret})).called(1);
   });
 }
