9aa905fc0711a6dc5050e943e9d6303ff3b1e5c2
Adolfo da Silveira Poiatti <adolfododo@hotmail.com>
2023-10-10 07:05:55 -0300

    move duplicated code to the setup method

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -6,25 +6,25 @@ import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:mobile_clean_tdd/domain/usecases/authentication.dart';
 
-
-
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
   late RemoteAuthentication sut;
   late HttpClientSpy httpClient;
   late String url;
+  late AuthenticationParams params;
 
   setUp(() {
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
     sut = RemoteAuthentication(httpClient: httpClient, url: url);
+    params = AuthenticationParams(
+        email: faker.internet.email(), secret: faker.internet.password());
   });
 
   test('should call http client with correct values', () async {
     //Act
-    final params = AuthenticationParams(
-        email: faker.internet.email(), secret: faker.internet.password());
+
     await sut.auth(params);
 
     //Assert
@@ -34,17 +34,18 @@ void main() {
         body: {'email': params.email, 'password': params.secret})).called(1);
   });
 
-    test('should throw UnexpectedError if httpClient returns 400', () async {
+  test('should throw UnexpectedError if httpClient returns 400', () async {
     //Arrange
-    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
-    .thenThrow(HttpError.badRequest);
-    final params = AuthenticationParams(
-        email: faker.internet.email(), secret: faker.internet.password());
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.badRequest);
+
     //Act
     final future = sut.auth(params);
 
     //Assert
     expect(future, throwsA(DomainError.unexpected));
-
   });
 }
